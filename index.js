d2eb0e83f42953e91babf984aaeb8e7565246f8d
chrispus1000 <minrevac@gmail.com>
2022-06-06 16:37:02 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'Someone is the champion';
+document.body.appendChild(newHeader);
